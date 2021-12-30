803750d9f13cc015d3130ab777073e4791d9ebcf
uniqcle <uniqcle@yandex.ru>
2021-12-30 12:19:30 +0700

    json web token - jwt

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -29,8 +29,21 @@ class LoginForm extends Form {
     doSubmit = async () => {
         // call the server
         //const username = this.username.current.value;
-        const { data } = this.state;
-        await login(data.username, data.password)
+        try {
+            const { data } = this.state;
+            const { data: jwt } = await login(data.username, data.password);
+
+            localStorage.setItem('token', jwt)
+
+            this.props.history.push("/")
+
+        } catch (ex) {
+            if (ex.response && ex.response.status === 400) {
+                const errors = { ...this.state.errors }
+                errors.username = ex.response.data;
+                this.setState({ errors })
+            }
+        }
     }
 
 
